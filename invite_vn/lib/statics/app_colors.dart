e2a1404d889903e729266b041540de3763e0791a
Phạm Như Vũ <phamnhuvu.dev@gmail.com>
2019-01-27 17:55:46 +0700

    add colors

---
+++ invite_vn/lib/statics/app_colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart' show Color, Colors;
+
+class AppColors {
+  static const HEADLINE_BODY = Color(0xFF000000);
+  static const SECONDARY = Color(0xFF666666);
+  static const FOOTNOTE_CAPTION = Color(0xFF8A8A8F);
+  static const CONTENT = Color(0xFFC8C7CC);
+  static const SCREEN = Color(0xFFEFEFF4);
+  static const BACKGROUND = Color(0xFFF9F9F9);
+  static const WHITE = Colors.white;
+  static const TORTOISE = Color(0xFF06BB9A);
+  static const RED = Color(0xFFF31111);
+
+  //PRIM GRADIENT
+  static const PRIM_GRA_START = Color(0xFF3AD9A9);
+  static const PRIM_GRA_END = Color(0xFF0D9090);
+}
