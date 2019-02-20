704b25efc00f2d8d8b035f653cc1bea04e39f4df
DavidSon <gnvisons@gmail.com>
2019-02-20 23:34:58 +0700

    Commit refact, tab user

--- invite_vn/lib/features/home/personal/personal_screen.dart
+++ invite_vn/lib/features/home/personal/personal_screen.dart
@@ -9,41 +9,79 @@ class PersonalScreen extends StatefulWidget {
 }
 
 class _StatePersonalScreen extends State<PersonalScreen> {
+
+  Widget bottomButton(){
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              height: 48.0,
+              width: 133.0,
+              margin: EdgeInsets.only(bottom: 20.0),
+              child: Center(
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Image.asset("assets/icons/ic_add.png"),
+                    Container(
+                      margin: EdgeInsets.only(left: 10.0),
+                      child: Text(
+                        'Tạo mới',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              decoration: BoxDecoration(
+                  boxShadow: [
+                    getShadow(),
+                  ],
+                  gradient: getGradient(),
+                  borderRadius: BorderRadius.circular(24.0)),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  BoxShadow getShadow(){
+    return BoxShadow(
+        color: Colors.black45,
+        blurRadius: 40.0,
+        offset: Offset(0.0, 5.0)
+    );
+  }
+
+  Gradient getGradient(){
+    return LinearGradient(
+      begin: Alignment.topRight,
+      end: Alignment.bottomLeft,
+      // Add one stop for each color. Stops should increase from 0 to 1
+      colors: [
+        // Colors are easy thanks to Flutter's Colors class.
+        Colors.green,
+        Colors.greenAccent,
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         key: PageStorageKey(Routes.PERSONAL),
         children: <Widget>[
-          Positioned(
-            bottom: 0,
-            child: Container(
-              height: 48.0,
-              width: 133.0,
-              child: Center(
-                child: Text(
-                  'Tạo mới',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20.0,
-                  ),
-                ),
-              ),
-              decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topRight,
-                    end: Alignment.bottomLeft,
-                    // Add one stop for each color. Stops should increase from 0 to 1
-                    colors: [
-                      // Colors are easy thanks to Flutter's Colors class.
-                      Colors.green,
-                      Colors.greenAccent,
-                    ],
-                  ),
-                  borderRadius: BorderRadius.circular(24.0)),
-            ),
-          )
+          bottomButton(),
         ],
       ),
     );
--- invite_vn/lib/widgets/item_communicate.dart
+++ invite_vn/lib/widgets/item_communicate.dart
@@ -3,6 +3,76 @@ import 'package:invite_vn/statics/routes.dart';
 import 'package:invite_vn/data/activity/activity.dart';
 
 class ItemCommunicate extends Scaffold {
+  Widget bacgroundShadow() {
+    return Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.bottomCenter,
+              end: Alignment.topCenter,
+              colors: [Color.fromRGBO(20, 20, 20, 0.5), Colors.transparent]),
+          borderRadius: BorderRadius.circular(8.0)),
+      margin: EdgeInsets.only(left: 0.0, top: 40.0, right: 0.0, bottom: 0.0),
+    );
+  }
+
+  Widget iconType() {
+    return Positioned(
+      child: Container(
+        margin: EdgeInsets.all(20.0),
+        width: 32,
+        height: 32,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8.0), color: Colors.red),
+      ),
+      top: 0,
+    );
+  }
+
+  Widget textCountPeople() {
+    return Positioned(
+      child: Container(
+        child: Center(
+          child: Text(
+            "2/4 người",
+            style:
+                TextStyle(color: Colors.white, fontWeight: FontWeight.normal),
+          ),
+        ),
+        margin: EdgeInsets.all(20.0),
+        padding: EdgeInsets.all(10.0),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8.0),
+            color: Color.fromRGBO(20, 20, 20, 0.5)),
+      ),
+      top: 0,
+      right: 0,
+    );
+  }
+
+  Text textName() {
+    return Text(
+      "Name",
+      style: TextStyle(
+          color: Colors.white, fontSize: 15.0, fontWeight: FontWeight.w600),
+    );
+  }
+
+  Text textActivity() {
+    return Text(
+      "Activity",
+      style: TextStyle(
+          color: Colors.white, fontSize: 20.0, fontWeight: FontWeight.bold),
+    );
+  }
+
+  Text textAddress() {
+    return Text(
+      "Địa chỉ",
+      style: TextStyle(
+          color: Colors.white, fontSize: 13.0, fontWeight: FontWeight.w500),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,50 +82,11 @@ class ItemCommunicate extends Scaffold {
           child: Stack(
             children: <Widget>[
               // gradient
-              Container(
-                decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                        begin: Alignment.bottomCenter,
-                        end: Alignment.topCenter,
-                        colors: [
-                          Color.fromRGBO(20, 20, 20, 0.5),
-                          Colors.transparent
-                        ]),
-                    borderRadius: BorderRadius.circular(8.0)),
-                margin: EdgeInsets.only(
-                    left: 0.0, top: 40.0, right: 0.0, bottom: 0.0),
-              ),
+              bacgroundShadow(),
               //* top icon
-              Positioned(
-                child: Container(
-                  margin: EdgeInsets.all(20.0),
-                  width: 32,
-                  height: 32,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8.0),
-                      color: Colors.red),
-                ),
-                top: 0,
-              ),
+              iconType(),
               //* right text
-              Positioned(
-                child: Container(
-                  child: Center(
-                    child: Text(
-                      "2/4 người",
-                      style: TextStyle(
-                          color: Colors.white, fontWeight: FontWeight.normal),
-                    ),
-                  ),
-                  margin: EdgeInsets.all(20.0),
-                  padding: EdgeInsets.all(10.0),
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8.0),
-                      color: Color.fromRGBO(20, 20, 20, 0.5)),
-                ),
-                top: 0,
-                right: 0,
-              ),
+              textCountPeople(),
               // * box bottom
               Positioned(
                 child: Container(
@@ -66,49 +97,33 @@ class ItemCommunicate extends Scaffold {
                       Row(
                         children: <Widget>[
                           Container(
-                              height: 48.0,
-                              width: 48.0,
-                              decoration: BoxDecoration(
-                                  color: Colors.red,
-                                  image: DecorationImage(
-                                    image: ExactAssetImage('img_tes/t.png'),
-                                    fit: BoxFit.cover,
-                                  ),
-                                  border: Border.all(
-                                      color: Colors.white, width: 2.0),
-                                  borderRadius: BorderRadius.circular(24.0))),
+                            height: 48.0,
+                            width: 48.0,
+                            decoration: BoxDecoration(
+                              color: Colors.red,
+                              image: DecorationImage(
+                                image: ExactAssetImage('img_tes/t.png'),
+                                fit: BoxFit.cover,
+                              ),
+                              border:
+                                  Border.all(color: Colors.white, width: 2.0),
+                              borderRadius: BorderRadius.circular(24.0),
+                            ),
+                          ),
                           Container(
                             margin: EdgeInsets.only(
                                 left: 12.0, top: 0.0, right: 0.0, bottom: 0.0),
                             child: Column(
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: <Widget>[
-                                Text(
-                                  "Name",
-                                  style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 15.0,
-                                      fontWeight: FontWeight.w600),
-                                ),
-                                Text(
-                                  "Activity",
-                                  style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 20.0,
-                                      fontWeight: FontWeight.bold),
-                                )
+                                textName(),
+                                textActivity(),
                               ],
                             ),
                           )
                         ],
                       ),
-                      Text(
-                        "Địa chỉ",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 13.0,
-                            fontWeight: FontWeight.w500),
-                      )
+                      textAddress(),
                     ],
                   ),
                 ),
